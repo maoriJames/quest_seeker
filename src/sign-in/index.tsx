4874ae22254005bce41c5c6b93f4b867f515772d
MLI Webdev <webdev@maorilandinfo.co.nz>
2025-09-09 16:09:05 +1200

    Add dayjs package

---
+++ src/sign-in/index.tsx
@@ -0,0 +1,6 @@
+// import { Navigate } from 'react-router-dom'
+
+export default function SignInPage() {
+  // return <Navigate href={'/(user)/region/'} />
+  return <div>Sign-in is working!</div>
+}
