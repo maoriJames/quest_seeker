397bcb09fb2180dc7a7a88d5061e83e94cacbfb6
MLI Webdev <webdev@maorilandinfo.co.nz>
2025-09-15 12:56:00 +1200

    Changes to userQuests while it deploys

--- src/hooks/userQuests.ts
+++ src/hooks/userQuests.ts
@@ -1,116 +1,118 @@
-import { client } from '@/lib/amplifyClient'
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { listQuests, getQuest, listProfiles } from '@/graphql/queries'
-import { createQuest, updateQuest, deleteQuest } from '@/graphql/mutations'
-import type { CreateQuestInput, UpdateQuestInput } from '@/API'
+// import { generateClient } from 'aws-amplify/api'
+// import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+// // import { listQuests, getQuest, listProfiles } from '@/graphql/queries'
+// // import { createQuest, updateQuest, deleteQuest } from '@/graphql/mutations'
+// // import type { CreateQuestInput, UpdateQuestInput } from '@/graphql/API'
 
-export const useQuestList = (region?: string) => {
-  return useQuery({
-    queryKey: ['quests', region],
-    queryFn: async () => {
-      const { data } = await client.graphql({
-        query: listQuests,
-        variables:
-          region && region !== 'Browse all'
-            ? { filter: { region: { eq: region } } }
-            : {},
-      })
-      return data.listQuests.items
-    },
-  })
-}
+// const client = generateClient()
 
-export const useQuest = (id: string) => {
-  return useQuery({
-    queryKey: ['quests', id],
-    queryFn: async () => {
-      const { data } = await client.graphql({
-        query: getQuest,
-        variables: { id },
-      })
-      return data.getQuest
-    },
-  })
-}
+// export const useQuestList = (region?: string) => {
+//   return useQuery({
+//     queryKey: ['quests', region],
+//     queryFn: async () => {
+//       const { data } = await client.graphql({
+//         query: listQuests,
+//         variables:
+//           region && region !== 'Browse all'
+//             ? { filter: { region: { eq: region } } }
+//             : {},
+//       })
+//       return data.listQuests.items
+//     },
+//   })
+// }
 
-export const useSeekerQuests = (userId: string) => {
-  return useQuery({
-    queryKey: ['seeker-quests', userId],
-    queryFn: async () => {
-      // 1. Fetch the profile
-      const { data } = await client.graphql({
-        query: listProfiles,
-        variables: { filter: { id: { eq: userId } } },
-      })
+// export const useQuest = (id: string) => {
+//   return useQuery({
+//     queryKey: ['quests', id],
+//     queryFn: async () => {
+//       const { data } = await client.graphql({
+//         query: getQuest,
+//         variables: { id },
+//       })
+//       return data.getQuest
+//     },
+//   })
+// }
 
-      const profile = data.listProfiles.items?.[0]
-      if (!profile) return []
+// export const useSeekerQuests = (userId: string) => {
+//   return useQuery({
+//     queryKey: ['seeker-quests', userId],
+//     queryFn: async () => {
+//       // 1. Fetch the profile
+//       const { data } = await client.graphql({
+//         query: listProfiles,
+//         variables: { filter: { id: { eq: userId } } },
+//       })
 
-      const questIds: string[] =
-        profile.my_quests?.map((q: any) => q.questId) ?? []
+//       const profile = data.listProfiles.items?.[0]
+//       if (!profile) return []
 
-      // 2. Fetch quests in parallel
-      const questResults = await Promise.all(
-        questIds.map((id) =>
-          client.graphql({
-            query: getQuest,
-            variables: { id },
-          })
-        )
-      )
+//       const questIds: string[] =
+//         profile.my_quests?.map((q: any) => q.questId) ?? []
 
-      return questResults.map((res) => res.data.getQuest).filter(Boolean)
-    },
-  })
-}
+//       // 2. Fetch quests in parallel
+//       const questResults = await Promise.all(
+//         questIds.map((id) =>
+//           client.graphql({
+//             query: getQuest,
+//             variables: { id },
+//           })
+//         )
+//       )
 
-export const useInsertQuest = () => {
-  const queryClient = useQueryClient()
+//       return questResults.map((res) => res.data.getQuest).filter(Boolean)
+//     },
+//   })
+// }
 
-  return useMutation({
-    mutationFn: async (questInput: CreateQuestInput) => {
-      const { data } = await client.graphql({
-        query: createQuest,
-        variables: { input: questInput },
-      })
-      return data.createQuest
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['quests'] })
-    },
-  })
-}
+// export const useInsertQuest = () => {
+//   const queryClient = useQueryClient()
 
-export const useUpdateQuest = () => {
-  const queryClient = useQueryClient()
+//   return useMutation({
+//     mutationFn: async (questInput: CreateQuestInput) => {
+//       const { data } = await client.graphql({
+//         query: createQuest,
+//         variables: { input: questInput },
+//       })
+//       return data.createQuest
+//     },
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({ queryKey: ['quests'] })
+//     },
+//   })
+// }
 
-  return useMutation({
-    mutationFn: async (questInput: UpdateQuestInput) => {
-      const { data } = await client.graphql({
-        query: updateQuest,
-        variables: { input: questInput },
-      })
-      return data.updateQuest
-    },
-    onSuccess: (_, { id }) => {
-      queryClient.invalidateQueries({ queryKey: ['quests'] })
-      queryClient.invalidateQueries({ queryKey: ['quests', id] })
-    },
-  })
-}
+// export const useUpdateQuest = () => {
+//   const queryClient = useQueryClient()
 
-export const useDeleteQuest = () => {
-  const queryClient = useQueryClient()
+//   return useMutation({
+//     mutationFn: async (questInput: UpdateQuestInput) => {
+//       const { data } = await client.graphql({
+//         query: updateQuest,
+//         variables: { input: questInput },
+//       })
+//       return data.updateQuest
+//     },
+//     onSuccess: (_, { id }) => {
+//       queryClient.invalidateQueries({ queryKey: ['quests'] })
+//       queryClient.invalidateQueries({ queryKey: ['quests', id] })
+//     },
+//   })
+// }
 
-  return useMutation({
-    mutationFn: async (id: string) => {
-      await client.graphql({
-        query: deleteQuest,
-        variables: { input: { id } },
-      })
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['quests'] })
-    },
-  })
-}
+// export const useDeleteQuest = () => {
+//   const queryClient = useQueryClient()
+
+//   return useMutation({
+//     mutationFn: async (id: string) => {
+//       await client.graphql({
+//         query: deleteQuest,
+//         variables: { input: { id } },
+//       })
+//     },
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({ queryKey: ['quests'] })
+//     },
+//   })
+// }
