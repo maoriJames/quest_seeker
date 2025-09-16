5833bdf76f515bed1585612e2aec0cc614fb2a35
MLI Webdev <webdev@maorilandinfo.co.nz>
2025-09-16 16:38:43 +1200

    Changes before switch

--- src/user/account/index.tsx
+++ src/user/account/index.tsx
@@ -41,6 +41,7 @@ export default function AccountPage() {
   }
 
   useEffect(() => {
+    if (!currentProfile) return
     const fetchCurrentId = async () => {
       try {
         console.log('What is the currentProfile', currentProfile)
