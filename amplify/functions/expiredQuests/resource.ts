5c633747cf21a85c9f75c1cef417e33e4759e32c
MLI Webdev <webdev@maorilandinfo.co.nz>
2025-12-11 16:23:48 +1300

    Add cron job to function for NZT

--- amplify/functions/expiredQuests/resource.ts
+++ amplify/functions/expiredQuests/resource.ts
@@ -1,6 +1,10 @@
 import { defineFunction } from '@aws-amplify/backend'
 
 export const expiredQuests = defineFunction({
-  name: 'expired-quests',
+  name: 'expiredQuests',
   entry: './handler.ts',
+  schedule: [
+    '0 12 * * ? *', // Midnight NZST (UTC+12)
+    '0 11 * * ? *', // Midnight NZDT (UTC+13)
+  ],
 })
