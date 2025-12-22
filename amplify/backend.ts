a1e1749d6035f6d828bddb9ed91be5ed0c9d8a7a
MLI Webdev <webdev@maorilandinfo.co.nz>
2025-12-23 12:45:36 +1300

    Add function for passing email from auth to profile on account creation

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend'
-import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources'
 import { StartingPosition } from 'aws-cdk-lib/aws-lambda'
@@ -8,6 +8,7 @@ import { data } from './data/resource'
 import { storage } from './storage/resource'
 import { expiredQuests } from './functions/expiredQuests/resource'
 import { sendNotification } from './functions/sendNotification/resource'
+import { postRegistration } from './functions/postRegistration/resource'
 
 const backend = defineBackend({
   auth,
@@ -15,26 +16,32 @@ const backend = defineBackend({
   storage,
   expiredQuests,
   sendNotification,
+  postRegistration,
 })
 
-const notificationLambda = backend.sendNotification.resources.lambda as lambda.Function;
+const notificationLambda = backend.sendNotification.resources
+  .lambda as lambda.Function
 
-const questTable = backend.data.resources.tables["Quest"]; 
-const profileTable = backend.data.resources.tables["Profile"]; // Access the Profile table
+const questTable = backend.data.resources.tables['Quest']
+const profileTable = backend.data.resources.tables['Profile'] // Access the Profile table
 
 // 1. Trigger from Quest table
-notificationLambda.addEventSource(new DynamoEventSource(questTable, {
-  startingPosition: StartingPosition.LATEST,
-}));
+notificationLambda.addEventSource(
+  new DynamoEventSource(questTable, {
+    startingPosition: StartingPosition.LATEST,
+  })
+)
 
 // 2. Grant SES permission
-notificationLambda.addToRolePolicy(new PolicyStatement({
-  actions: ["ses:SendEmail", "ses:SendRawEmail"],
-  resources: ["*"],
-}));
+notificationLambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['ses:SendEmail', 'ses:SendRawEmail'],
+    resources: ['*'],
+  })
+)
 
 // 3. Grant DynamoDB Read permission for the Profile table
-profileTable.grantReadData(notificationLambda);
+profileTable.grantReadData(notificationLambda)
 
 // 4. Pass the Profile table name to the Lambda environment
-notificationLambda.addEnvironment("PROFILE_TABLE_NAME", profileTable.tableName);
+notificationLambda.addEnvironment('PROFILE_TABLE_NAME', profileTable.tableName)
